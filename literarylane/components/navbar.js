c94f956754d3157c005a2ad357b0d7216cf055c5
AustinMerilos <67397590+AustinMerilos@users.noreply.github.com>
2023-05-21 19:15:01 -0700

    chore: styling for header and navbar

--- literarylane/components/navbar.js
+++ literarylane/components/navbar.js
@@ -1,13 +1,63 @@
-import Link from "next/link";
 import React from "react";
+import { styled } from "styled-components";
+import Link from "next/link";
 
+// Styled navigation bar container
+const NavBar = styled.nav`
+  background-color: #333;
+  padding: 20px;
+`;
+
+// Styled navigation bar list
+const NavList = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: center;
+`;
+
+// Styled navigation bar list item
+const NavItem = styled.li`
+  margin: 0 10px;
+`;
+
+// Styled navigation bar link
+const NavLink = styled.p`
+  color: #fff;
+  text-decoration: none;
+  padding: 5px 10px;
+  border-radius: 5px;
+
+  &:hover {
+    background-color: #555;
+  }
+`;
+
+// Navbar component
 export default function Navbar() {
   return (
-    <nav>
-      <Link href={"/products"}>Products</Link>
-      <Link href={"/sell"}>Sell</Link>
-      <Link href={"/account"}>Account</Link>
-      <Link href={"/orders"}>Orders</Link>
-    </nav>
+    <NavBar>
+      <NavList>
+        <NavItem>
+          <Link href="/products">
+            <NavLink>Products</NavLink>
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link href="/sell">
+            <NavLink>Sell</NavLink>
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link href="/account">
+            <NavLink>Account</NavLink>
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link href="/orders">
+            <NavLink>Orders</NavLink>
+          </Link>
+        </NavItem>
+      </NavList>
+    </NavBar>
   );
 }
--- literarylane/pages/_app.js
+++ literarylane/pages/_app.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Home from ".";
 import Header from "../components/header";
-import Pages from "../components/pages";
 
 export default function App({ Component }) {
   return (
