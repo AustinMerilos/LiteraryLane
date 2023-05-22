a3f61dc9ef17584b04e33bd9998ec69cce989736
AustinMerilos <67397590+AustinMerilos@users.noreply.github.com>
2023-05-22 14:38:36 -0700

    chore: removed or moved files

--- literarylane/components/pages.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Pages(props) {
-  return <div>{props.someProperty}</div>;
-}
--- literarylane/pages/sell/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function SellPage() {
-  return <div>sell</div>;
-}
