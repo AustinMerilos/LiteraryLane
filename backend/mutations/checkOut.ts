5ab3a2c693799d3e74172b7212b9c74d1bfe5379
AustinMerilos <67397590+AustinMerilos@users.noreply.github.com>
2024-01-11 14:16:49 -0800

    chore: create order and orderItems in checkout mutation

--- backend/mutations/checkOut.ts
+++ backend/mutations/checkOut.ts
@@ -74,6 +74,33 @@ async function checkout(
       throw new Error(err.message);
     });
   console.log(charge);
+  //cartItems change to orderItems
+  const orderItems = cartItems.map((cartItem) => {
+    const orderItem = {
+      name: cartItem.product.name,
+      description: cartItem.product.description,
+      price: cartItem.product.price,
+      quantity: cartItem.quantity,
+      photo: { connect: { id: cartItem.product.photo.id } },
+    };
+    return orderItem;
+  });
+  //order
+  const order = await context.lists.Order.createOne({
+    data: {
+      total: charge.amount,
+      charge: charge.id,
+      items: { create: orderItems },
+      user: { connect: { id: userId } },
+    },
+    resolveFields: false,
+  });
+
+  const cartItemIds = cartItems.map((cartItem) => cartItem.id);
+  await context.lists.CartItem.deleteMany({
+    ids: cartItemIds,
+  });
+  return order;
 }
 
 export default checkout;
